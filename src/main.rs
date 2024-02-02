d13e6999269825a494ed12438dd2ed8e51ab6285
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-02-02 17:46:05 +0000

    Add notes to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // there is a 5-year cycle
-// INPUT - previous exam mark schemes - Question/Answers/Extra Information/Mark/(AO / Spec. Ref.) - last 2 columns are important (pdf)
+// look at how many marks as well e.g. many 2 markers, not come up as a 6 marker in 5 years etc.
+// INPUT - (pdf) previous exam mark schemes - Question/Answers/Extra Information/Mark/(AO / Spec. Ref.) - last 2 columns are important
 // INPUT - specification which lists all topics (e.g. 4.2.4.1)
 // OUTPUT - the likelihood of topics (e.g. 4.2.4.1) coming up in the next exam
 
