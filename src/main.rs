9f43591bf5deb126f4de43f465ec4d4c1147933e
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-02-02 17:45:13 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// there is a 5-year cycle
+// INPUT - previous exam mark schemes - Question/Answers/Extra Information/Mark/(AO / Spec. Ref.) - last 2 columns are important (pdf)
+// INPUT - specification which lists all topics (e.g. 4.2.4.1)
+// OUTPUT - the likelihood of topics (e.g. 4.2.4.1) coming up in the next exam
+
+fn main() {
+    println!("Hello, world!");
+}
