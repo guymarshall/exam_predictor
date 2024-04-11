948cf93bea32a828aaa9a04ba6432f141943c3fc
Guy Marshall <guymarshall9@gmail.com>
2024-04-11 14:42:53 +0100

    UPDATE: Remove notes from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-// there is a 5-year cycle
-// look at how many marks as well e.g. many 2 markers, not come up as a 6 marker in 5 years etc.
-// INPUT - (pdf) previous exam mark schemes - Question/Answers/Extra Information/Mark/(AO / Spec. Ref.) - last 2 columns are important
-// INPUT - specification which lists all topics (e.g. 4.2.4.1)
-// OUTPUT - the likelihood of topics (e.g. 4.2.4.1) coming up in the next exam
-
 fn main() {
-    println!("Use .docx files instead of .pdf files (need converting maybe in this program?");
+    println!("Hello, World!");
 }
