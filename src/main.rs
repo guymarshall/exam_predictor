217ea22b616105da9c2077fa408aae2e07693048
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-12-14 23:42:59 +0000

    WIP: Code to get codes from specifications errors due to crate issue

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,18 @@ mod test_pdf;
 
 use codes::{get_code_counts, get_codes};
 use file_reader::get_pdf_filenames;
+use specification_pdf::SpecificationPDF;
 use subject::{extract_subject_from_filename, Subject};
 use test_pdf::TestPDF;
 
 fn main() {
-    let subjects: Vec<Subject> = get_pdf_filenames("specifications")
+    let specification_filenames: Vec<String> = get_pdf_filenames("specifications");
+    let subjects: Vec<Subject> = specification_filenames
         .iter()
         .map(|filename: &String| extract_subject_from_filename(filename))
         .collect();
 
-    subjects.iter().for_each(|subject: &Subject| {
+    for subject in subjects {
         let code: i32 = subject.get_code();
         let filenames: Vec<String> = get_pdf_filenames("tests")
             .into_iter()
@@ -26,9 +28,17 @@ fn main() {
             println!("Subject: {subject}");
         }
 
-        // TODO: for each subject -> get full list of codes from specifications
+        let specification_filename: String = specification_filenames
+            .clone()
+            .into_iter()
+            .filter(|filename: &String| filename.contains(&code.to_string()))
+            .last()
+            .unwrap();
+        ////! ERROR in crate: PdfError(Decryption(UnsupportedEncryption)) for specifications due to PDF-1.7 format being used. PDF-1.6 format works fine.
+        // let specification_codes: Vec<String> = get_codes(&specification_filename);
+        // let specification_pdf: SpecificationPDF =
+        //     SpecificationPDF::new(specification_filename, subject.clone(), specification_codes);
         // TODO: for each subject -> get list of codes that are missing from the PDFs
-
         let test_pdfs: Vec<TestPDF> = filenames
             .into_iter()
             .map(|filename: String| {
@@ -44,5 +54,5 @@ fn main() {
             .for_each(|test_pdf: &TestPDF| println!("{}", test_pdf));
 
         // TODO: write list of codes in descending order to file for each subject e.g. biology.txt
-    });
+    }
 }
