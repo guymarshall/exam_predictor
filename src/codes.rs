82c06118ef3bfafeef17d5ba1db366ee80b6b1d0
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-12-14 19:12:53 +0000

    UPDATE: Refactor codes code into codes.rs

---
+++ src/codes.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+use pdf_extract::extract_text;
+
+const DISALLOWED_CHARACTERS: [char; 55] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '–', '/', ' ',
+];
+
+const VALID_CHARACTERS: [char; 11] = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+pub(crate) fn get_codes(filename: &String) -> Vec<String> {
+    let text: String = extract_text(filename).unwrap();
+
+    let codes: Vec<String> = text
+        .lines()
+        .map(|line: &str| line.trim().to_string())
+        .filter(|line: &String| line.contains(VALID_CHARACTERS))
+        .filter(|line: &String| !line.contains(DISALLOWED_CHARACTERS))
+        .filter(|line: &String| line.len() == 5 || line.len() == 7)
+        .filter(|line: &String| line.matches('.').count() == 2 || line.matches('.').count() == 3)
+        .collect();
+
+    codes
+}
+
+pub(crate) fn get_code_counts(codes: &[String]) -> Vec<(String, i32)> {
+    let counts: HashMap<String, i32> = codes.iter().fold(
+        HashMap::new(),
+        |mut accumulator: HashMap<String, i32>, pattern: &String| {
+            *accumulator.entry(pattern.to_string()).or_insert(0) += 1;
+            accumulator
+        },
+    );
+
+    let mut sorted_counts: Vec<_> = counts.into_iter().collect();
+    sorted_counts.sort_by(|a: &(String, i32), b: &(String, i32)| b.1.cmp(&a.1));
+    sorted_counts
+}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,18 @@
+mod codes;
 mod specification_pdf;
 mod subject;
 mod test_pdf;
 
-use pdf_extract::extract_text;
-use std::collections::HashMap;
+use codes::{get_code_counts, get_codes};
 use std::ffi::OsStr;
 use std::fs;
 use std::path::PathBuf;
 use subject::{extract_subject_from_filename, Subject};
 use test_pdf::TestPDF;
 
-const DISALLOWED_CHARACTERS: [char; 55] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '–', '/', ' ',
-];
-
-const VALID_CHARACTERS: [char; 11] = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-const TESTS_DIRECTORY: &str = "tests";
-
-fn get_codes(filename: &String) -> Vec<String> {
-    let text: String = extract_text(filename).unwrap();
-
-    let codes: Vec<String> = text
-        .lines()
-        .map(|line: &str| line.trim().to_string())
-        .filter(|line: &String| line.contains(VALID_CHARACTERS))
-        .filter(|line: &String| !line.contains(DISALLOWED_CHARACTERS))
-        .filter(|line: &String| line.len() == 5 || line.len() == 7)
-        .filter(|line: &String| line.matches('.').count() == 2 || line.matches('.').count() == 3)
-        .collect();
-
-    codes
-}
-
-fn get_code_counts(codes: &[String]) -> Vec<(String, i32)> {
-    let counts: HashMap<String, i32> = codes.iter().fold(
-        HashMap::new(),
-        |mut accumulator: HashMap<String, i32>, pattern: &String| {
-            *accumulator.entry(pattern.to_string()).or_insert(0) += 1;
-            accumulator
-        },
-    );
-
-    let mut sorted_counts: Vec<_> = counts.into_iter().collect();
-    sorted_counts.sort_by(|a: &(String, i32), b: &(String, i32)| b.1.cmp(&a.1));
-    sorted_counts
-}
-
 fn main() {
     // TODO: do this for each subject individually
-    let filenames: Vec<String> = fs::read_dir(TESTS_DIRECTORY)
+    let filenames: Vec<String> = fs::read_dir("tests")
         .unwrap()
         .map(|file: Result<fs::DirEntry, std::io::Error>| file.unwrap().path())
         .filter(|path: &PathBuf| path.extension() == Some(OsStr::new("pdf")))
