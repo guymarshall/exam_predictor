e50d53c8fbcc658e4b81a3d25c8cb47beccbbf15
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-12-18 22:24:27 +0000

    UPDATE: Pass extension to more generic get_filenames()

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,6 +1,6 @@
 use std::{ffi::OsStr, fs, path::PathBuf};
 
-pub(crate) fn get_pdf_filenames(path: &str) -> Vec<String> {
+pub(crate) fn get_filenames(path: &str, extension: &str) -> Vec<String> {
     let filenames: Vec<String> = fs::read_dir(path)
         .expect("Failed to read directory")
         .map(|file: Result<fs::DirEntry, std::io::Error>| file.expect("Failed to get file").path())
@@ -8,7 +8,7 @@ pub(crate) fn get_pdf_filenames(path: &str) -> Vec<String> {
             path.extension()
                 .expect("Failed to get extension")
                 .to_ascii_lowercase()
-                == OsStr::new("pdf")
+                == OsStr::new(extension)
         })
         .map(|path: PathBuf| path.to_string_lossy().to_string())
         .collect();
